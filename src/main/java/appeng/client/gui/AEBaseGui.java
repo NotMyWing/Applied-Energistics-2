7c67762d068a30bb2be94cdc40d4bd18263de97c
jchung01 <jjchung276@gmail.com>
2024-06-11 17:35:27 -0700

    Fix some MouseTweaks interactions

--- src/main/java/appeng/client/gui/AEBaseGui.java
+++ src/main/java/appeng/client/gui/AEBaseGui.java
@@ -44,6 +44,7 @@ import appeng.util.Platform;
 import appeng.util.item.AEItemStack;
 import com.google.common.base.Joiner;
 import com.google.common.base.Stopwatch;
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 import mezz.jei.api.gui.IGhostIngredientHandler;
 import net.minecraft.client.Minecraft;
@@ -1057,7 +1058,7 @@ public abstract class AEBaseGui extends GuiContainer implements IMTModGuiContain
     @Override
     @Optional.Method(modid = "mousetweaks")
     public boolean MT_isMouseTweaksDisabled() {
-        return true;
+        return false;
     }
 
     @Override
@@ -1087,12 +1088,20 @@ public abstract class AEBaseGui extends GuiContainer implements IMTModGuiContain
     @Override
     @Optional.Method(modid = "mousetweaks")
     public boolean MT_isIgnored(Slot slot) {
-        return true;
+        return false;
     }
 
     @Override
     @Optional.Method(modid = "mousetweaks")
     public boolean MT_disableRMBDraggingFunctionality() {
-        return true;
+       if (this.dragSplitting && this.dragSplittingButton == 1) {
+           this.dragSplitting = false;
+           // Don't ignoreMouseUp on slots that can't accept the item. (crafting output, ME slot, etc.)
+           if (this.getSlotUnderMouse() != null && this.getSlotUnderMouse().isItemValid(this.mc.player.inventory.getItemStack())) {
+               this.ignoreMouseUp = true;
+           }
+           return true;
+       }
+       return false;
     }
 }
