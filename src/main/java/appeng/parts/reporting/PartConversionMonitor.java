6a1890658d9ee0b1f65c4e485747b309aa19622b
zeng-git <95841646+zeng-github01@users.noreply.github.com>
2024-06-13 23:53:09 +0800

    Add Gas Support for Conversion Monitor

--- src/main/java/appeng/parts/reporting/PartConversionMonitor.java
+++ src/main/java/appeng/parts/reporting/PartConversionMonitor.java
@@ -39,6 +39,12 @@ import appeng.parts.PartModel;
 import appeng.util.InventoryAdaptor;
 import appeng.util.Platform;
 import appeng.util.item.AEItemStack;
+import com.mekeng.github.common.me.data.IAEGasStack;
+import com.mekeng.github.common.me.data.impl.AEGasStack;
+import com.mekeng.github.common.me.storage.IGasStorageChannel;
+import com.mekeng.github.util.Utils;
+import com.mekeng.github.util.helpers.ItemGasHandler;
+import mekanism.api.gas.GasStack;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
@@ -95,6 +101,7 @@ public class PartConversionMonitor extends AbstractPartMonitor {
 
         final ItemStack eq = player.getHeldItem(hand);
         FluidStack fluidInTank = null;
+        Object gasInTank = null;
         if (eq.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null)) {
             IFluidHandlerItem fluidHandlerItem = (eq.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY, null));
             fluidInTank = fluidHandlerItem.drain(Integer.MAX_VALUE, false);
@@ -110,6 +117,16 @@ public class PartConversionMonitor extends AbstractPartMonitor {
                 if (this.getDisplayed() != null && getDisplayed().equals(AEFluidStack.fromFluidStack(fluidInTank))) {
                     this.drainFluidContainer(player, hand);
                 }
+            } else if (Platform.isModLoaded("mekeng")) {
+                ItemGasHandler itemGasHandler = Utils.getItemGasHandler(eq);
+                if (itemGasHandler != null) {
+                    gasInTank = itemGasHandler.removeGas(Integer.MAX_VALUE, false);
+                    if (gasInTank != null && ((GasStack) gasInTank).amount > 0) {
+                        if (this.getDisplayed() != null && this.getDisplayed().equals(AEGasStack.of(((GasStack) gasInTank)))) {
+                            this.drainGasContainer(player, hand);
+                        }
+                    }
+                }
             } else {
                 this.insertItem(player, hand, false);
             }
@@ -122,6 +139,15 @@ public class PartConversionMonitor extends AbstractPartMonitor {
                 return super.onPartActivate(player, hand, pos);
             }
             if (((IAEFluidStack) this.getDisplayed()).equals(AEFluidStack.fromFluidStack(fluidInTank))) {
+                this.drainFluidContainer(player, hand);
+            } else {
+                return super.onPartActivate(player, hand, pos);
+            }
+        } else if (Platform.isModLoaded("mekeng") && gasInTank != null && ((GasStack) gasInTank).amount > 0) {
+            if (getDisplayed() instanceof IAEItemStack || getDisplayed() == null) {
+                return super.onPartActivate(player, hand, pos);
+            }
+            if (((IAEGasStack) this.getDisplayed()).equals(AEFluidStack.fromFluidStack(fluidInTank))) {
                 this.drainFluidContainer(player, hand);
             } else {
                 return super.onPartActivate(player, hand, pos);
@@ -150,6 +176,10 @@ public class PartConversionMonitor extends AbstractPartMonitor {
             this.extractItem(player, ((IAEItemStack) this.getDisplayed()).getDefinition().getMaxStackSize());
         } else if (this.getDisplayed() != null && this.getDisplayed() instanceof IAEFluidStack) {
             this.fillFluidContainer(player, hand);
+        }
+
+        if (Platform.isModLoaded("mekeng") && this.getDisplayed() != null && this.getDisplayed() instanceof IAEGasStack) {
+            this.fillGasContainer(player, hand);
         }
 
         return true;
@@ -360,6 +390,120 @@ public class PartConversionMonitor extends AbstractPartMonitor {
                 AELog.error("Fluid item [%s] reported a different possible amount than it actually accepted.", held.getDisplayName());
             }
             player.setHeldItem(hand, fh.getContainer());
+        } catch (GridAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void drainGasContainer(final EntityPlayer player, final EnumHand hand) {
+        try {
+            final ItemStack held = player.getHeldItem(hand);
+            if (held.getCount() != 1) {
+                // only support stacksize 1 for now
+                return;
+            }
+
+            final ItemGasHandler itemGasHandler = Utils.getItemGasHandler(held);
+            if (itemGasHandler == null) {
+                // only gas handlers items
+                return;
+            }
+
+            // See how much we can drain from the item
+            final GasStack extract = itemGasHandler.removeGas(Integer.MAX_VALUE, false);
+            if (extract == null || extract.amount < 1) {
+                return;
+            }
+
+            // Check if we can push into the system
+            final IEnergySource energy = this.getProxy().getEnergy();
+            final IMEMonitor<IAEGasStack> cell = this.getProxy()
+                    .getStorage()
+                    .getInventory(
+                            AEApi.instance().storage().getStorageChannel(IGasStorageChannel.class));
+            final IAEGasStack notStorable = Platform.poweredInsert(energy, cell, AEGasStack.of(extract), new PlayerSource(player, this), Actionable.SIMULATE);
+
+            if (notStorable != null && notStorable.getStackSize() > 0) {
+                final int toStore = (int) (extract.amount - notStorable.getStackSize());
+                final GasStack storable = itemGasHandler.removeGas(toStore, false);
+
+                if (storable == null || storable.amount == 0) {
+                    return;
+                } else {
+                    extract.amount = storable.amount;
+                }
+            }
+
+            // Actually drain
+            final GasStack drained = itemGasHandler.removeGas(extract, true);
+            extract.amount = drained.amount;
+
+            final IAEGasStack notInserted = Platform.poweredInsert(energy, cell, AEGasStack.of(extract), new PlayerSource(player, this));
+
+            if (notInserted != null && notInserted.getStackSize() > 0) {
+                AELog.error("Fluid item [%s] reported a different possible amount to drain than it actually provided.", held.getDisplayName());
+            }
+
+            player.setHeldItem(hand, itemGasHandler.getContainer());
+        } catch (GridAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillGasContainer(final EntityPlayer player, final EnumHand hand) {
+        try {
+            final ItemStack held = player.getHeldItem(hand);
+            if (held.getCount() != 1) {
+                // only support stacksize 1 for now
+                return;
+            }
+
+            final ItemGasHandler itemGasHandler = Utils.getItemGasHandler(held);
+            if (itemGasHandler == null) {
+                // only gas handlers items
+                return;
+            }
+
+            final IAEGasStack stack = (IAEGasStack) this.getDisplayed().copy();
+
+            // Check how much we can store in the item
+            stack.setStackSize(Integer.MAX_VALUE);
+            int amountAllowed = itemGasHandler.addGas(stack.getGasStack(), false);
+            stack.setStackSize(amountAllowed);
+
+            // Check if we can pull out of the system
+            final IEnergySource energy = this.getProxy().getEnergy();
+            final IMEMonitor<IAEGasStack> cell = this.getProxy()
+                    .getStorage()
+                    .getInventory(
+                            AEApi.instance().storage().getStorageChannel(IGasStorageChannel.class));
+            final IAEGasStack canPull = Platform.poweredExtraction(energy, cell, stack, new PlayerSource(player, this), Actionable.SIMULATE);
+            if (canPull == null || canPull.getStackSize() < 1) {
+                return;
+            }
+
+            // How much could fit into the container
+            final int canFill = itemGasHandler.addGas(canPull.getGasStack(), false);
+            if (canFill == 0) {
+                return;
+            }
+
+            // Now actually pull out of the system
+            stack.setStackSize(canFill);
+            final IAEGasStack pulled = Platform.poweredExtraction(energy, cell, stack, new PlayerSource(player, this));
+            if (pulled == null || pulled.getStackSize() < 1) {
+                // Something went wrong
+                AELog.error("Unable to pull fluid out of the ME system even though the simulation said yes ");
+                return;
+            }
+
+            // Actually fill
+            final int used = itemGasHandler.addGas(pulled.getGasStack(), true);
+
+            if (used != canFill) {
+                AELog.error("Fluid item [%s] reported a different possible amount than it actually accepted.", held.getDisplayName());
+            }
+            player.setHeldItem(hand, itemGasHandler.getContainer());
         } catch (GridAccessException e) {
             e.printStackTrace();
         }
